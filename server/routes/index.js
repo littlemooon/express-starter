68e182fbe2732e22d9e12df7bf785fbf8cfa5b57
littlemooon <fredwright0@gmail.com>
2017-02-19 20:57:42 +0100

    :sparkles: sequelize

--- server/routes/index.js
+++ server/routes/index.js
@@ -11,10 +11,10 @@ export default app => {
 
   app.get('/api/message', controllers.message.list)
   app.get('/api/message/:messageId', controllers.message.retrieve)
+
   app.post('/api/user/:userId/message', controllers.message.create)
   app.put('/api/user/:userId/message/:messageId', controllers.message.update)
   app.delete('/api/user/:userId/message/:messageId', controllers.message.destroy)
-
   app.all('/api/user/:userId/message', (req, res) => res.status(405).send({
     message: 'Method Not Allowed',
   }))
